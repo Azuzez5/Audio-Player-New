7776d15679f36bc01f60efc41a5b8c238be82f95
Azuzez5 <phamanhkhoa1708ltv@gmail.com>
2026-03-14 07:38:03 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,22 @@
-const CACHE_NAME = "audio-player-cache"
+const CACHE_NAME = "audio-player-cache-v1"
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/icon.svg"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js"
 ]
 
 self.addEventListener("install", event => {
 
   event.waitUntil(
-
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+      })
+      .catch(err => {
+        console.log("Cache failed:", err)
+      })
   )
 
 })
@@ -23,10 +24,10 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
 
   event.respondWith(
-
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-
+      .then(response => {
+        return response || fetch(event.request)
+      })
   )
 
 })
